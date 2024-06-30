a0533128ec3ec6fcb700a51a8859f1cbd1c82c5b
sinasezza <s.e.sezza121@gmail.com>
2024-06-30 15:44:50 +0330

    home page added ant test endpoints added to tests directory.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request, status
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
+
+BASE_DIR = pathlib.Path(__file__).parent
 
 app = FastAPI()
 
 
-@app.get("/")
-def home_view():
-    return {"message": "Welcome to the FastAPI application!"}
+templates = Jinja2Templates(directory=str(BASE_DIR / 'templates'))
+
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    # return {"message": "Welcome to the FastAPI application!"}
+    return templates.TemplateResponse("home.html", context={"request": request})
 
 
 @app.post("/")
